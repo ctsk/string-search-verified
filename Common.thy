dab440aea4af07794c7f2dac92033631e2608287
Christian <9384305+ctsk@users.noreply.github.com>
2022-07-29 17:45:37 +0200

    Naive String Search now returns the indices at which the substring is found

---
+++ Common.thy
@@ -0,0 +1,12 @@
+theory Common
+  imports Main
+begin
+
+\<comment> \<open>Yanked from AFP/Knuth_MorrisPratt.KMP\<close>
+fun sublist_at :: "'a list \<Rightarrow> 'a list \<Rightarrow> nat \<Rightarrow> bool" where
+  "sublist_at (x#xs) (y#ys) 0 \<longleftrightarrow> x=y \<and> sublist_at xs ys 0" |
+  "sublist_at xs (y#ys) (Suc i) \<longleftrightarrow> sublist_at xs ys i" |
+  "sublist_at [] ys 0 \<longleftrightarrow> True" |
+  "sublist_at _ [] _ \<longleftrightarrow> False"
+
+end
--- Naive.thy
+++ Naive.thy
@@ -1,59 +1,54 @@
 theory Naive
-  imports "HOL-Library.Sublist"
+  imports Common "HOL-Library.Sublist"
 begin
 
-
-fun is_prefix :: "string \<Rightarrow> string \<Rightarrow> bool" where
+fun is_prefix :: "'a list \<Rightarrow> 'a list \<Rightarrow> bool" where
   "is_prefix [] str = True"
 | "is_prefix pref [] = False"
 | "is_prefix (p#pref) (s#str) = (if s = p then is_prefix pref str else False)"
 
-
 text \<open>naive_search [needle] [haystack]\<close>
-fun naive_search :: "string \<Rightarrow> string \<Rightarrow> bool" where
-  "naive_search [] hay = True"
-| "naive_search needle [] = False"
-| "naive_search needle (h#ay) = 
+fun naive_search ::  "'a list \<Rightarrow> 'a list \<Rightarrow> nat list" where
+  "naive_search [] hay = [0]"
+| "naive_search needle [] = []"
+| "naive_search needle (h#ay) =
     (if is_prefix needle (h#ay)
-     then True
-     else naive_search needle ay)"
+     then 0 # (map Suc (naive_search needle ay))
+     else map Suc (naive_search needle ay))"
 
-value "naive_search ''hello'' ''hello world''"
+value "naive_search ''hello'' ''hello world hello'' = [0, 12]"
+value "naive_search ''a'' ''aba'' = [0, 2]"
 
 lemma is_prefix_correct: "is_prefix needle hay = prefix needle hay"
   by (induction hay rule: is_prefix.induct) auto
 
-lemma naive_search_correct: "naive_search needle hay = sublist needle hay"
-proof (induction hay rule: naive_search.induct)
+lemma is_prefix_substring_at: "is_prefix needle hay = sublist_at needle hay 0"
+  by (induction hay rule: is_prefix.induct) auto
+
+lemma naive_search_correct:
+  assumes "idx \<in> set (naive_search needle hay)"
+    shows "sublist_at needle hay idx"
+  using assms
+proof (induction hay arbitrary: idx rule: naive_search.induct)
   case (1 hay)
   then show ?case by simp
 next
   case (2 n eedle)
   then show ?case by simp
 next
-  case IH: (3 n eedle h ay)
+  case (3 n eedle h ay idx)
+
   let ?needle = "n#eedle"
   let ?hay = "h#ay"
-  show ?case
-  proof (cases "is_prefix ?needle ?hay")
-    case prefix: True
 
-    have 0: "naive_search ?needle ?hay"
-      using prefix by auto
+  have "idx \<in> set (map Suc (naive_search ?needle ay)) \<Longrightarrow> sublist_at ?needle ?hay idx"
+    using "3.IH" by fastforce
 
-    have 1: "sublist ?needle ?hay"
-      using is_prefix_correct prefix by blast
-
-    show ?thesis using 0 1 by blast
-  next
-    case notprefix: False
-
-    then have "naive_search ?needle ay = sublist ?needle ay"
-      using IH notprefix by blast
-
-    then show ?thesis
-      by (metis is_prefix_correct naive_search.simps(3) sublist_Cons_right)
-  qed
+  then show ?case 
+    using is_prefix_substring_at 
+    by (metis "3.prems" naive_search.simps(3) set_ConsD)
 qed
+
+
 
 end
